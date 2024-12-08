bb4712200249dfd2658dbdd48265a8e8c9ca8039
Aizina <113802373+Aizina@users.noreply.github.com>
2024-12-08 15:37:35 +0200

    deploy2

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  output: 'export',
+  basePath: '/BookShopNext', 
+  assetPrefix: '/BookShopNext/',
   reactStrictMode: true,
   images: {
     domains: ["books.google.com"],
